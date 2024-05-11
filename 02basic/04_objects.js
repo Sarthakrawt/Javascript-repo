10df07f62c8470ebd5d81b598fc89738132fb3ac
Sarthakrawt <155871591+Sarthakrawt@users.noreply.github.com>
2024-05-12 00:06:40 +0530

    Update 04_objects.js

--- 02basic/04_objects.js
+++ 02basic/04_objects.js
@@ -1,10 +1,15 @@
+
+//advance object methods
+// assigning new object using new constructor
 const tinder = new Object();
 const tinderUser ={};
+//add properties
 tinderUser.name = "sammy";
 tinderUser.id ="sammy123";
 
 console.log((tinderUser));
 
+// object inside an object
 const regularUser = {
     email: " some@gmail.com",
     fullname: {
@@ -23,6 +28,8 @@ const obj2={
     name:"saara",
     age:23
 }
+// using of sperator operators
+// obj3 contains both the properties in obj1, obj2
 const obj3 ={...obj1, ...obj2};
 console.log(obj3);
 
@@ -32,7 +39,8 @@ console.log(obj3);
 const obj4 = Object.assign({}, obj1,obj2);
 console.log(obj4);
 
-
+// arrays is also an object 
+// inside which you can also store other object and data types
 const arr=[
     {name: "sara",
 email: "sara@gmail.com"
@@ -41,19 +49,22 @@ email: "sara@gmail.com"
 email: "doe@gmail.com"
 }
 ]
-console.log(Object.keys(arr));
-console.log(Object.values(arr));
-console.log(Object.entries(arr));
-console.log(Object.hasOwnProperty('name'));
+// using these method you can print keys , values , entries 
+console.log(Object.keys(arr));//index
+console.log(Object.values(arr));// name, email and its values
+console.log(Object.entries(arr));//index name , email and its values
+console.log(Object.hasOwnProperty('name'));// true
 
  
 
 // destructuring of objects
+// destructure the object property 
 const course = {
     coursename :"js in hindi",
     price: "12423",
     courseInstructor: "hitesh"
 }
+// this is destructuring of object
 const {courseInstructor: Instructor} = course;
 // console.log(courseInstructor);
 //this is callled destructuring
