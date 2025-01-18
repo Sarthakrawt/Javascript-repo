9f66b7574f546aba8cf6e86f197f1096236ba292
Sarthak Singh Rawat <sachinrumal@gmail.com>
2025-01-18 15:53:36 +0530

    Update 07Stringadv.js

--- basic/07Stringadv.js
+++ basic/07Stringadv.js
@@ -5,15 +5,16 @@ const name = "samaphone";
 
 console.log(` name: ${name}`);
 
-// this is call stringinterpolation which advaced then the normal "quotes"
+// this is call stringinterpolation which advance then the normal "quotes"
 
 const gamename = new String('hemant-');
 
 //string is an object 
+// when we use new keyword mean it will create an object for us
 
 console.log(typeof gamename);// object 
 
-console.log(gamename[0]);// javascript start with 0 indexing 
+console.log(gamename[0]);// javascript start with 0 index
 
 console.log(gamename.length);
 
