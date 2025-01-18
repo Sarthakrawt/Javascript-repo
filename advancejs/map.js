d80f0c29937956df661eb9cbedf1a1c0e4d5bedb
Sarthak Singh Rawat <sachinrumal@gmail.com>
2025-01-18 15:58:20 +0530

    Update map.js

--- advancejs/map.js
+++ advancejs/map.js
@@ -7,7 +7,8 @@
 // console.log(value);// for each don't return a value just return undefined
 
 const myNums = [1,2,3,4,5,6,7];
-
+const a = myNums.map(val => val);
+ console.log(a) // will give you all the element in the array 
 const newNums = myNums.filter((num)=> num > 5);// it will give you number greater than 5
 console.log(newNums);
 
